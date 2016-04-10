af95cb093c85c189adc09b1b2bc490b2cae6006c
Joensg <joen.sg@gmail.com>
2016-04-10 20:13:49 +0100

    added angular ui-router and views

--- App_files/app/scripts/app.js
+++ App_files/app/scripts/app.js
@@ -1,3 +1,47 @@
 'use strict';
 
-angular.module('rpsApp', []);
+angular.module('rpsApp', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    
+    // route for the home page
+        .state('app', {
+            url:'/',
+            views: {
+                'header': {
+                    templateUrl : 'views/header.html',
+                },
+                'content': {
+                    templateUrl : 'views/home.html',
+                    controller  : 'GameController'
+                },
+                'footer': {
+                    templateUrl : 'views/footer.html',
+                }
+            }
+        })
+    
+    // route for the game page
+        .state('app.game', {
+            url:'game',
+            views: {
+                'content@': {
+                    templateUrl : 'views/game.html',
+                    controller  : 'GameController'
+                }
+            }
+        })
+    
+    // route for the gamelog page
+        .state('app.gamelog', {
+            url:'gamelog',
+            views: {
+                'content@': {
+                    templateUrl : 'views/gamelog.html',
+                    controller  : 'GameController'
+                }
+            }
+        });
+    
+    $urlRouterProvider.otherwise('/');
+});
