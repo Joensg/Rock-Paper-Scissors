5f1ac44323ba2e84ccd6880747155ea1a295bee4
Joensg <joen.sg@gmail.com>
2017-04-02 16:47:31 +0100

    changed to point to correct server address.

--- App_files/app/scripts/services/loginService.js
+++ App_files/app/scripts/services/loginService.js
@@ -2,7 +2,7 @@
 
 angular
     .module('rpsApp')
-    .constant("baseURL", "http://ec2-52-49-151-42.eu-west-1.compute.amazonaws.com:8080/") //server IP address
+    .constant("baseURL", "http://ec2-52-56-244-250.eu-west-2.compute.amazonaws.com:8080/") //server IP address
     .factory('loginFactory', ['$resource', 'baseURL', '$localStorage', function($resource, baseURL, $localStorage) {
         var loginfac = {};
 
